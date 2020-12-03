e6b5939bcc0a97019a56f66528f8d43650801f8f
hyundang <hyunjin5697@yonsei.ac.kr>
2020-12-03 19:20:17 +0900

    주석 작성

--- client.py
+++ client.py
@@ -16,10 +16,6 @@ class ChatRoom(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.btnLogin = QPushButton(self)
-        self.btnLogin.setText('Login')
-        self.btnLogin.pressed.connect(self.onLoginClick)
-
         self.lb1 = QLabel(self)
         self.lb1.setText('server IP')
         self.lb3 = QLabel(self)
@@ -33,19 +29,27 @@ class ChatRoom(QWidget):
         self.inputPort = QLineEdit(self)
         self.inputName = QLineEdit(self)
         self.inputPass = QLineEdit(self)
-        
-        self.le = QLineEdit(self)
-        self.le.returnPressed.connect(self.getText)
+        # ip주소, port 번호, 이름, password 입력 UI
+
+        self.btnLogin = QPushButton(self)
+        self.btnLogin.setText('Login')
+        self.btnLogin.pressed.connect(self.onLoginClick)
+        # 로그인 버튼 UI
+
 
         self.tb = QTextBrowser(self)
         self.tb.setAcceptRichText(True)
         self.tb.setOpenExternalLinks(True)
+        # 채팅창 UI
 
-        self.btnSend = QPushButton('Send')
-        self.btnFile = QPushButton('File')
+        self.le = QLineEdit(self)
+        self.le.returnPressed.connect(self.getText)
+        # 채팅 입력칸 UI
+
         self.btnQuit = QPushButton('Quit')
         self.btnQuit.pressed.connect(self.onQuit)
-        
+        # quit 버튼 UI
+
 
         vbox = QVBoxLayout()
         vbox.addWidget(self.lb1)
@@ -59,8 +63,6 @@ class ChatRoom(QWidget):
         vbox.addWidget(self.btnLogin)
         vbox.addWidget(self.tb)
         vbox.addWidget(self.le)
-        vbox.addWidget(self.btnSend)
-        vbox.addWidget(self.btnFile)
         vbox.addWidget(self.btnQuit)
 
         self.setLayout(vbox)
@@ -70,21 +72,31 @@ class ChatRoom(QWidget):
         self.resize(800, 800)
         self.show()
 
+
     def onLoginClick(self):
         self.host = self.inputIP.text()
         self.port = self.inputPort.text()
         self.name = self.inputName.text()
         self.password = self.inputPass.text()
+        # ip, port, name, password 문자열 가져옴
+
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((self.host, int(self.port)))
+        # 가져온 ip, port 값으로 socket 연결
+
         runChat(self, self.sock)
+
         self.sock.send((self.name+' '+self.password).encode())
+        # 서버에 이름과 비밀번호 보냄
+        
         self.start = time.time()
+        # 시간 측정 시작
+        
         global isLogin 
         isLogin = True
         self.inputName.clear()
         self.inputPass.clear()
-        # print(self.start)
+        # 로그인이 성공했다는 표시하고, 이름과 비밀번호 칸 초기화
 
 
     def onQuit(self):
@@ -92,26 +104,35 @@ class ChatRoom(QWidget):
         if isLogin:
           self.sock.send('quit'.encode())
           isLogin = False
-        #   self.inputPort.clear()
-        #   self.inputIP.clear()
+        # 만약 채팅 종료가 이미 된 상태라면 실행 안됨.
+        # 누르면 서버로 'quit'을 보내고, 채팅 종료됨.
+
+
 
     def getText(self):
         global TimeLimit
         
         self.timeTerm = time.time() - self.start
+        # 새로 채팅을 보내기까지 걸린 시간 측정
 
         if float(self.timeTerm) > TimeLimit:
-            print('timeover')
+            # print('timeover')
             self.sock.send('timeover'.encode())
             self.inputName.setText(self.name)
             self.inputPass.setText(self.password)
             global isLogin
             isLogin = False
+            # 채팅 텀이 TimeLimit 이상이라면 강제로 종료됨.
+            # 로그인도 해제되었다고 표시
+            # 이름, 비밀번호 창에 기존 값 넣어줌
         else:
             self.message = self.le.text()
             self.sock.send(self.message.encode())
             self.start = time.time()
+            # 채팅 텀이 TimeLimit 이하라면 정상적으로 채팅 작동.
+            # 시간 측정 재시작
         self.le.clear()
+
 
 
 
@@ -121,11 +142,13 @@ def rcvMsg(sock, ex):
             data = sock.recv(1024)
             if not data:
                 break
-            print('Received from the server :',repr(data.decode()))
+            # print('Received from the server :',repr(data.decode()))
             ex.tb.append(data.decode())
+            # 서버에서 오는 값을 받아서 채팅창 UI에 출력
         except:
             print('fail')
             pass
+
 
 def runChat(ex, sock):
     t = Thread(target=rcvMsg, args=(sock,ex))
--- server.py
+++ server.py
@@ -9,111 +9,117 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtCore import QObject, QThread, QMutex
 import time
 
-# HOST = '127.0.0.1'
-# PORT = 9999
+
 lock = threading.Lock() # syncronized 동기화 진행하는 스레드 생성
-# mutex = QMutex()
 
 PASSWORD = 0
 isFalse = True
 isSame = False
 TimeOverUsers = []
 
-class UserManager: 
-            # 사용자관리 및 채팅 메세지 전송을 담당하는 클래스
-            # ① 채팅 서버로 입장한 사용자의 등록
-            # ② 채팅을 종료하는 사용자의 퇴장 관리
-            # ③ 사용자가 입장하고 퇴장하는 관리
-            # ④ 사용자가 입력한 메세지를 채팅 서버에 접속한 모두에게 전송
 
+class userHandle: 
    def __init__(self):
-        self.users = {} # 사용자의 등록 정보를 담을 사전 {사용자 이름:(소켓,주소),...}
+        self.users = {} 
+        # 사용자 정보가 담길 dictionary
+        # {username: (socket, address),...} 형태
 
-   def addUser(self, username, password, conn, addr): # 사용자 ID를 self.users에 추가하는 함수
+   def addUser(self, username, password, conn, addr): # username, socket, address를 users에 추가하는 함수
         global isFalse
         global TimeOverUsers 
         global isSame       
-        if isFalse:
-            if password != PASSWORD:
+        if isFalse: # 입력된 비밀번호가 잘못되었을 때
+            if password != PASSWORD: 
                 conn.send('비밀번호가 잘못되었습니다.\n'.encode())
                 return None
-        isFalse = False
         
-        if username in self.users: # 이미 등록된 사용자라면
-            if str(username) in TimeOverUsers: # 강제로 튕긴 사용자라면
+        isFalse = False # 입력된 비밀번호가 올바를 때
+        
+        if username in self.users: # 이미 등록된 user라면
+            if str(username) in TimeOverUsers: # 강제로 종료된 user라면
                 # print('재접속')
-                if username in TimeOverUsers:
+                if username in TimeOverUsers:  # 강제로 종료된 user 목록에서 username 삭제
                     TimeOverUsers.remove(username)
-                lock.acquire() # 스레드 동기화를 막기위한 락
-                self.users[username] = (conn, addr) #(소켓, 주소)
-                lock.release() # 업데이트 후 락 해제
-                self.sendMessageToAll('[%s]님이 재접속 되었습니다.\n' %username)
-                print('+++ 대화 참여자 수 [%d]' %len(self.users))
+                
+                lock.acquire() # 스레드 동기화를 막기위한 lock
+                self.users[username] = (conn, addr) #users에 추가
+                lock.release() # 업데이트 후 lock 해제
+                
+                self.SendMsgAll('[%s]님이 재접속 되었습니다.\n' %username)
+                print('사용자 입장: 대화 참여자 수 [%d]' %len(self.users))
                 return username
-            else:
+            else: # 이미 등록된 user
                 isSame = True
                 conn.send('이미 등록된 사용자입니다.\n'.encode())
                 return None
         
-        if username == 'quit':
+        if username == 'quit': # 유저가 quit 버튼 눌렀을 때
             if username in TimeOverUsers:
                 TimeOverUsers.remove(username)
             return None
 
+
+        # 만약 새로운 사용자라면
         # 새로운 사용자를 등록함
-        lock.acquire() # 스레드 동기화를 막기위한 락
-        self.users[username] = (conn, addr) #(소켓, 주소)
-        lock.release() # 업데이트 후 락 해제
+        # lock으로 동기화 방지
+        lock.acquire() 
+        self.users[username] = (conn, addr)
+        lock.release() 
 
-        # self.users.append
 
-        self.sendMessageToAll('[%s]님이 입장했습니다.' %username)
-        print('+++ 대화 참여자 수 [%d]' %len(self.users))
+        self.SendMsgAll('[%s]님이 입장했습니다.' %username)
+        print('사용자 입장: 대화 참여자 수 [%d]' %len(self.users))
             
         return username
 
-   def removeUser(self, username): #사용자를 제거하는 함수
+
+
+   def deleteUser(self, username): #users에서 사용자 삭제
         if username not in self.users:
             return
 
+        # users에서 사용자 삭제
+        # lock으로 동기화 방지
         lock.acquire()
         del self.users[username]
         lock.release()
 
-        self.sendMessageToAll('[%s]님이 퇴장했습니다.' %username)
-        print('--- 대화 참여자 수 [%d]' %len(self.users))
+        self.SendMsgAll('[%s]님이 퇴장했습니다.' %username)
+        print('사용자 퇴장: 대화 참여자 수 [%d]' %len(self.users))
+
+
 
    def messageHandler(self, username, msg): # 전송한 msg를 처리하는 부분
         global TimeOverUsers
 
-        if msg.strip() == 'quit': # 보낸 메세지가 'quit'이면
-            self.removeUser(username)
+        if msg.strip() == 'quit': # 유저가 quit 버튼 눌렀을 때
+            self.deleteUser(username) # 채팅 종료
+            print('사용자 퇴장: 대화 참여자 수 [%d]' %len(self.users))
             return -1
             
-        if msg.strip() == 'timeover':
-            # self.removeUser(username)
+        if msg.strip() == 'timeover': # TimeLimit 초과했을 때 채팅 강제 종료
+            # self.deleteUser(username)
             # 전역 배열을 만들어서 여기에 username 저장하고,
             # registeruser 부분에서 이 배열에 만약에 저장되어 있는 애면
             # addr만 바꿔줌
             if username not in TimeOverUsers:
                 TimeOverUsers.append(username)
-            # print(TimeOverUsers)
-            print('--- 대화 참여자 수 [%d]' %len(self.users))
+            print('사용자 퇴장: 대화 참여자 수 [%d]' %len(self.users))
             return -2
 
         if msg[0] != '/': # 보낸 메세지의 첫문자가 '/'가 아니면
-            self.sendMessageToAll('[%s] %s' %(username, msg))
+            self.SendMsgAll('[%s] %s' %(username, msg))
             return
 
     
 
-   def sendMessageToAll(self, msg):
+   def SendMsgAll(self, msg):  # 채팅방에 접속한 모든 사용자에게 메세지 전달하는 함수
       for conn, addr in self.users.values():
          conn.send(msg.encode())
           
 
-class MyTcpHandler(socketserver.BaseRequestHandler):
-   userman = UserManager()
+class SocketHandle(socketserver.BaseRequestHandler):
+   userman = userHandle()
     
    def handle(self): # 클라이언트가 접속시 클라이언트 주소 출력
         print('[%s] 연결됨' %self.client_address[1])
@@ -123,46 +129,43 @@ class MyTcpHandler(socketserver.BaseRequestHandler):
         isSame = False
         try:
             username = self.registerUsername()
-            # print(username)
-            if username == 'quit':
+            if username == 'quit': # 유저가 quit 버튼 눌렀을 때
                 self.request.send('채팅이 종료되었습니다.\n'.encode())
                 self.request.close()
-            elif username == 'false':
+            elif username == 'false': # username이 이미 등록한 이름일 때
                 self.request.close()
             else:
                 msg = self.request.recv(1024)
                 while msg:
-                    # print(msg.decode())
                     x = self.userman.messageHandler(username, msg.decode())
-                    if x == -1 or x == -2:
-                        if x == -1:
+                    if x == -1 or x == -2: # 채팅 종료하는 경우
+                        if x == -1: # 유저가 quit 버튼 눌렀을 때
                             self.request.send('채팅이 종료되었습니다.\n'.encode())
                             self.request.close()
-                        else:
+                        else: # 유저가 시간 초과되어 강제종료 될 때
                             self.request.send('시간이 초과되었습니다'.encode())
-                            self.userman.sendMessageToAll('[%s]님의 연결이 끊어졌습니다.\n' %username)
+                            self.userman.SendMsgAll('[%s]님의 연결이 끊어졌습니다.\n' %username)
                             self.request.close()        
                         break
                     msg = self.request.recv(1024)
                     
         except Exception as e:
             print(e)
-            self.userman.removeUser(username)
+            self.userman.deleteUser(username)
 
         print('[%s] 접속종료' %self.client_address[1])
-        # self.userman.removeUser(username)
+        # self.userman.deleteUser(username)
 
    def registerUsername(self):
         while True:
             global isFalse
-            # self.request.send('로그인ID:'.encode())
             username = self.request.recv(1024)
-            # password = self.request.recv(1024)
             username = username.decode().strip()
-            # password = password.decode().strip()
             arr = username.split(' ')
             username = arr[0]
             password = arr[1]
+            # 클라이언트로 부터 온 이름과 비밀번호 분리
+
             if self.userman.addUser(username, int(password), self.request, self.client_address):
                 return username
             if isFalse:
@@ -170,26 +173,23 @@ class MyTcpHandler(socketserver.BaseRequestHandler):
             if isSame:
                 return 'false'
 
-class ChatingServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+class MakeServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
         
 
 
 if __name__ == '__main__':
-#    runServer(ex)
-#    app = QApplication(sys.argv)
-#    ex = Server(app)
-#    ex = Window()
-#    sys.exit(app.exec_())
     HOST = input("IP 주소를 입력하세요: ")
     PORT = input("port 번호를 입력하세요: ")
     PASS = input("password를 입력하세요: ")
     PASSWORD = int(PASS)
+    # ip, port, name, password 초기값 설정 
+
     print("run server")
     try:
-        server= ChatingServer((HOST, int(PORT)), MyTcpHandler)
+        server= MakeServer((HOST, int(PORT)), SocketHandle)
         server.serve_forever()
-    except KeyboardInterrupt:
+    except KeyboardInterrupt: #ctrl+c 누르면 서버 종료
         print("stop server")
         server.shutdown()
         server.server_close()
